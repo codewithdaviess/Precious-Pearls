6088c64b9a6df83da21359af911aba90c5494c1f
codewithdaviess <codewithdaviess@gmail.com>
2025-06-18 23:49:16 +0200

    Resized logo

--- pearls/src/components/Navbar.jsx
+++ pearls/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-center">
-        <img src={logo} alt="Precious Pearls Logo" className="h-10" />
+        <img src={logo} alt="Precious Pearls Logo" className="h-16" />
       </div>
     </nav>
   );
